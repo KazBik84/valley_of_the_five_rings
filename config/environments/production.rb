1faefc85ffe7d66542cf01bfaf0d19ac3afdf028
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-01-10 06:53:49 -0400

    Switched host address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'moja-apka.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://gentle-garden-2759.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   # ENV ms_username and ms_pass are set in config/applicaion.yml
   ActionMailer::Base.smtp_settings = {
