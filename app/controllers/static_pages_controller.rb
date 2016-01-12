632686f6a1cfc8bd5aaf1d7f171e68d8e2bd76de
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-01-12 19:47:00 -0400

    Added Static Pages controller allong with contact method with the view

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+# This controller holds simple pages, that are not related to any models
+# or objects.
+class StaticPagesController < ApplicationController
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'announcements#index'
   devise_for :users
   resources :announcements, only: [:index]
+  get 'contact', to: 'static_pages#contact'
 end
