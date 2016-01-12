33d5f7b5920d8b7d97891c8aa72a0f9484ce15ec
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-01-12 19:46:21 -0400

    Added method that validates active class based on the provide active_header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  # Returns base title or title with current page name
+  def title_function(title = '')
+    if title.empty?
+      return 'Dolina Pięciu Kręgów'
+    else
+      return "Dolina Pięciu Kręgów | #{title}"
+    end
+  end
+
+  # Checks if nav bar item is active
+  def active_check(nav_name, name)
+    nav_name == name ? 'active' : nil
+  end
+
+  def bootstrap_class_for(flash_type)
+    { success: 'alert-success', error: 'alert-danger',
+      alert: 'alert-warning', notice: 'alert-info' }
+    [flash_type] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
+              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+              concat message 
+            end)
+    end
+    nil
+  end  
 end
