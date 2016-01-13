22447773d688ae5cc73c749ac6983c744850e244
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-01-13 18:25:40 -0400

    Added flash messages with stying

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,11 +13,10 @@ module ApplicationHelper
     nav_name == name ? 'active' : nil
   end
 
-  def bootstrap_class_for(flash_type)
-    { success: 'alert-success', error: 'alert-danger',
-      alert: 'alert-warning', notice: 'alert-info' }
-    [flash_type] || flash_type.to_s
-  end
+def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-error", alert: "alert-warning",
+     notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
