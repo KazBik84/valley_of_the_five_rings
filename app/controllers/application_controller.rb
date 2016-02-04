095b433d5c7f1d7e58141dddc17f43c77cfd778c
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-02-04 17:05:57 -0400

    Added rescue function from cancan access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from CanCan::AccessDenied do
+    flash[:alert] = 'Access Denied'
+    redirect_to root_path
+  end
 
-protected
+  protected
 
 
   def configure_permitted_parameters
