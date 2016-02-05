e74c7940be8509215ea0eaca158b5a9946dbff00
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-02-05 16:31:50 -0400

    Controllers test after cancan added

--- spec/controllers/announcements_controller_spec.rb
+++ spec/controllers/announcements_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe AnnouncementsController, type: :controller do
+  not_allowed_roles = [0, 1, 3]
   describe 'Get #new' do
     context 'user not logged in' do
       before(:each) do
@@ -14,9 +15,9 @@ RSpec.describe AnnouncementsController, type: :controller do
       end
     end
 
-    context 'user log in' do
+    context 'user admin log in' do
       before(:each) do
-        @user = create(:user)
+        @user = create(:user, role: 2)
         sign_in @user
         get :new
       end
@@ -27,19 +28,37 @@ RSpec.describe AnnouncementsController, type: :controller do
         expect(response).to render_template 'new'
       end
     end
+    not_allowed_roles.each do |role_id|
+      context 'user other then adnim log in' do
+        before(:each) do
+          @user = create(:user, role: role_id)
+          sign_in @user
+          get :new
+        end
+        it 'returns redirect http status' do
+          expect(response).to have_http_status(:redirect)
+        end
+        it 'redirect to root' do
+          expect(response).to redirect_to root_path
+        end
+        it 'shows Access Denied flash' do
+          expect(flash[:alert]).to eq 'Access Denied'
+        end
+      end
+    end
   end
 
   describe 'POST #create' do
-    context 'with valid attributes' do
+    context 'with valid attributes and with authority' do
       before(:each) do
-        @user = create(:user)
+        @user = create(:user, role: 2)
         sign_in @user
         post :create, announcement: attributes_for(:announcement)
       end
       it 'return http redirect status' do
         expect(response).to have_http_status(:redirect)
       end
-      it 'redirect to root path (aha index)' do
+      it 'redirect to root path' do
         expect(response).to redirect_to root_path
       end
       it 'will rise flash[:success]' do
@@ -49,10 +68,31 @@ RSpec.describe AnnouncementsController, type: :controller do
         expect(Announcement.count). to eq(1)
       end
     end
+    not_allowed_roles.each do |role_id|
+      context 'with valid attributes and with invalid authority' do
+        before(:each) do
+          @user = create(:user, role: role_id)
+          sign_in @user
+          post :create, announcement: attributes_for(:announcement)
+        end
+        it 'return http redirect status' do
+          expect(response).to have_http_status(:redirect)
+        end
+        it 'redirect to root path' do
+          expect(response).to redirect_to root_path
+        end
+        it 'will rise flash[:success]' do
+          expect(flash[:success]).to_not be_present
+        end
+        it 'will create announcement object' do
+          expect(Announcement.count). to eq(0)
+        end
+      end
+    end
 
-    context 'with title nil' do
+    context 'admin create announcement with title nil' do
       before(:each) do
-        @user = create(:user)
+        @user = create(:user, role: 2)
         sign_in @user
         post :create, announcement: attributes_for(:announcement, title: nil)
       end
@@ -67,9 +107,9 @@ RSpec.describe AnnouncementsController, type: :controller do
       end
     end
 
-    context 'with message nil' do
+    context 'admin create announcement with message nil' do
       before(:each) do
-        @user = create(:user)
+        @user = create(:user, role: 2)
         sign_in @user
         post :create, announcement: attributes_for(:announcement, message: nil)
       end
@@ -89,31 +129,48 @@ RSpec.describe AnnouncementsController, type: :controller do
     let(:dummy_post) do
       create(:announcement)
     end
-    before(:each) do
-      @user = create(:user)
-      sign_in @user
-      get :edit, id: dummy_post.id
+    context 'admin edites the announcement' do
+      before(:each) do
+        @user = create(:user, role: 2)
+        sign_in @user
+        get :edit, id: dummy_post.id
+      end
+      it 'should have success status' do
+        expect(response).to have_http_status(:success)
+      end
+      it 'should render edit template' do
+        expect(response).to render_template :edit
+      end
+      it 'assigns announcement variable correct' do
+        expect(assigns(:announcement)).to eq(dummy_post)
+      end
     end
-    it 'should have success status' do
-      expect(response).to have_http_status(:success)
-    end
-    it 'should render edit template' do
-      expect(response).to render_template :edit
-    end
-    it 'assigns announcement variable correct' do
-      expect(assigns(:announcement)).to eq(dummy_post)
+    not_allowed_roles.each do |role_id|
+      context 'not allowed user try to edit announcement' do
+        before(:each) do
+          @user = create(:user, role: role_id)
+          sign_in @user
+          get :edit, id: dummy_post.id
+        end
+        it 'should have redirect status' do
+          expect(response).to have_http_status(:redirect)
+        end
+        it 'should render edit template' do
+          expect(response).to redirect_to root_path
+        end
+      end    
     end
   end
   describe 'POST #update' do
     let(:dummy_post) do
       create(:announcement)
     end
-    context 'update with valid attributes' do
+    context 'admin update with valid attributes' do
       let(:attributes) do
         { title: 'new title', message: 'new message' }
       end
       before(:each) do
-        @user = create(:user)
+        @user = create(:user, role: 2)
         sign_in @user
         put :update, id: dummy_post.id, announcement: attributes
       end
@@ -136,12 +193,12 @@ RSpec.describe AnnouncementsController, type: :controller do
       end
     end
 
-    context 'update with blank title' do
+    context 'admin update with blank title' do
       let(:attributes) do
         { title: nil, message: 'new message' }
       end
       before(:each) do
-        @user = create(:user)
+        @user = create(:user, role: 2)
         sign_in @user
         put :update, id: dummy_post.id, announcement: attributes
       end
@@ -160,12 +217,12 @@ RSpec.describe AnnouncementsController, type: :controller do
         expect(dummy_post.message).to_not eq 'new message'
       end
     end
-    context 'update with blank message' do
+    context 'admin update with blank message' do
       let(:attributes) do
         { title: 'new title', message: nil }
       end
       before(:each) do
-        @user = create(:user)
+        @user = create(:user, role: 2)
         sign_in @user
         put :update, id: dummy_post.id, announcement: attributes
       end
@@ -182,6 +239,35 @@ RSpec.describe AnnouncementsController, type: :controller do
       it 'should not update message' do
         dummy_post.reload
         expect(dummy_post.message).to_not eq 'new message'
+      end
+    end
+    not_allowed_roles.each do |role_id|
+      context 'not allowed user try to update announcement' do
+        let(:attributes) do
+          { title: 'new title', message: 'new message' }
+        end
+        before(:each) do
+          @user = create(:user, role: role_id)
+          sign_in @user
+          put :update, id: dummy_post.id, announcement: attributes
+        end
+        it 'should have status redirect' do
+          expect(response).to have_http_status :redirect
+        end
+        it 'should redirect to root path' do
+          expect(response).to redirect_to root_path
+        end
+        it 'should have flash message' do
+          expect(flash[:alert]).to eq('Access Denied')
+        end
+        it 'should update the title' do
+          dummy_post.reload
+          expect(dummy_post.title).to eq(dummy_post.title)
+        end
+        it 'should update message' do
+          dummy_post.reload
+          expect(dummy_post.message).to eq(dummy_post.message)
+        end
       end
     end
   end
@@ -190,22 +276,45 @@ RSpec.describe AnnouncementsController, type: :controller do
     let(:dummy_post) do
       create(:announcement)
     end
-    before(:each) do
-      @user = create(:user)
-      sign_in @user
-      delete :destroy, id: dummy_post.id
+    context 'admin try to delete announcement' do
+      before(:each) do
+        @user = create(:user, role: 2)
+        sign_in @user
+        delete :destroy, id: dummy_post.id
+      end
+      it 'returns redirect status' do
+        expect(response).to have_http_status :redirect
+      end
+      it 'redirects to root path' do
+        expect(response).to redirect_to root_path
+      end
+      it 'have flash success' do
+        expect(flash[:success]).to be_present
+      end
+      it 'destroys an announcement' do
+        expect(Announcement.count).to eq(0)
+      end
     end
-    it 'returns redirect status' do
-      expect(response).to have_http_status :redirect
-    end
-    it 'redirects to root path' do
-      expect(response).to redirect_to root_path
-    end
-    it 'have flash success' do
-      expect(flash[:success]).to be_present
-    end
-    it 'destroys an announcement' do
-      expect(Announcement.count).to eq(0)
+    not_allowed_roles.each do |role_id|
+      context 'not allowed user try to delete announcement' do
+        before(:each) do
+          @user = create(:user, role: role_id)
+          sign_in @user
+          delete :destroy, id: dummy_post.id
+        end
+        it 'should have status redirect' do
+          expect(response).to have_http_status :redirect
+        end
+        it 'should redirect to root path' do
+          expect(response).to redirect_to root_path
+        end
+        it 'should have flash message' do
+          expect(flash[:alert]).to eq('Access Denied')
+        end
+        it 'should not destroy announcement' do
+          expect(Announcement.count).to eq(1)
+        end
+      end
     end
   end
 
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  roles_able_to_create_comments = [0, 1, 2]
+  roles_able_to_destory_comments = [1, 2]
+
   describe 'User not logged in' do
     let(:dummy_post) do
       create(:announcement)
@@ -9,7 +12,6 @@ RSpec.describe CommentsController, type: :controller do
       post :create, announcement_id: dummy_post.id,
                     comment: attributes_for(:comment)
     end
-
     it 'return redirect http status' do
       expect(response).to have_http_status(:redirect)
     end
@@ -23,41 +25,43 @@ RSpec.describe CommentsController, type: :controller do
     let(:dummy_post) do
       create(:announcement)
     end
-    context 'With valid attributes' do
-      before :each do
-        @user = create(:user)
-        sign_in @user
-        post :create, comment: attributes_for(:comment),
-                      announcement_id: dummy_post.id
-      end
+    roles_able_to_create_comments.each do |role_id|
+      context 'authorized user create comment with valid attributes' do
+        before :each do
+          @user = create(:user, role: role_id)
+          sign_in @user
+          post :create, comment: attributes_for(:comment, user_id: @user.id),
+                        announcement_id: dummy_post.id
+        end
 
-      it 'returns http status redirect' do
-        expect(response).to have_http_status(:redirect)
+        it 'returns http status redirect' do
+          expect(response).to have_http_status(:redirect)
+        end
+        it 'redirects to the given url' do
+          expect(response).to redirect_to(root_path)
+        end
+        it 'will create a comment in the db' do
+          expect(Comment.count).to eq(1)
+        end
       end
-      it 'redirects to the given url' do
-        expect(response).to redirect_to(root_path)
-      end
-      it 'will create a comment in the db' do
-        expect(Comment.count).to eq(1)
-      end
-    end
+      context 'With nil content' do
+        before :each do
+          @user = create(:user, role: role_id)
+          sign_in @user
+          post :create, comment: attributes_for(:comment, content: nil, user_id: @user.id),
+                        announcement_id: dummy_post.id
 
-    context 'With nil content' do
-      before :each do
-        @user = create(:user)
-        sign_in @user
-        post :create, comment: attributes_for(:comment, content: nil),
-                      announcement_id: dummy_post.id
-      end
+        end
 
-      it 'returns http status success' do
-        expect(response).to have_http_status(:success)
-      end
-      it 'renders index announcement view' do
-        expect(response).to render_template 'announcements/index'
-      end
-      it 'does not create Comment' do
-        expect(Comment.count).to eq(0)
+        it 'returns http status success' do
+          expect(response).to have_http_status(:success)
+        end
+        it 'renders index announcement view' do
+          expect(response).to render_template 'announcements/index'
+        end
+        it 'does not create Comment' do
+          expect(Comment.count).to eq(0)
+        end
       end
     end
   end
