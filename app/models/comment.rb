a33b21b2ffedb85bfc7cbe98237372c5c94fab82
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-02-09 00:31:36 -0400

    Set comment belongs_to user relation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :comment_owner, polymorphic: true
+  belongs_to :user
 
   validates_presence_of :content, :user_id
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :comments, as: :comment_owner
+  has_many :comments
+
   
   # Include default devise modules. Others available are:
   # , :timeoutable and :omniauthable
