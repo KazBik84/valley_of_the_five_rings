738bc83c0b3cdbfca74bdda5569565245f5a0472
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-02-12 01:14:12 -0400

    Added legendopedia basic css along with testing side menu

---
+++ app/controllers/legendopedia_controller.rb
@@ -0,0 +1,10 @@
+# This controller will be used to manage all actions
+#  related to the hard data from the seed file.
+class LegendopediaController < ApplicationController
+  def main
+    @show_legendopedia_panel = true        
+  end
+
+  def show_clan
+  end
+end
