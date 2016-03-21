0eb8fab00e86d167b8e2794c2c7b8b98ce392d66
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-03-21 02:08:02 -0300

    Finished refsactoring of all legendopedia views

--- app/helpers/legendopedia_helper.rb
+++ app/helpers/legendopedia_helper.rb
@@ -1,7 +1,7 @@
 module LegendopediaHelper
 
-  def join_names_attr_with_comma(hash)
-    hash.map { |x| x[:name]}.join(', ')
+  def join_names_attr_with_comma(hash, attr_name)
+    hash.map { |x| x[attr_name]}.join(', ')
   end
 
   def write_skill(school_skill, skill_list)
