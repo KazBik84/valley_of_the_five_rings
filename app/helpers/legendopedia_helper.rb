d7b57fdb6c49cd6e8bd2ac2837f31ad1c61fc354
Kazimierz Bik <kazimierz.bik@gmail.com>
2016-03-21 02:00:46 -0300

    Finished refactoring show_clan view

--- app/helpers/legendopedia_helper.rb
+++ app/helpers/legendopedia_helper.rb
@@ -3,4 +3,11 @@ module LegendopediaHelper
   def join_names_attr_with_comma(hash)
     hash.map { |x| x[:name]}.join(', ')
   end
+
+  def write_skill(school_skill, skill_list)
+    skill = school_skill.skill.name_pl
+    school_skill.school_emphasis.present? ? skill += " (#{school_skill.school_emphasis})" : ""
+    school_skill.value != 1 ? skill += " #{school_skill.value}" : ""
+    school_skill == skill_list.last ? skill : skill += ","
+  end
 end
